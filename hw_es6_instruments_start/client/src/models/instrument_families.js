dadb507d615ace409aaf32bf71050b9f7a5ad2ef
Stephen Rafferty <srafferty73@gmail.com>
2019-02-18 09:38:08 +0000

    completed

---
+++ hw_es6_instruments_start/client/src/models/instrument_families.js
@@ -0,0 +1,43 @@
+// const PubSub = require('../helpers/pub_sub.js');
+import PubSub from '../helpers/pub_sub.js';
+
+// const InstrumentFamilies = function (data) {
+//   this.data = data;
+// };
+
+class InstrumentFamilies{
+  constructor(data){
+    this.data = data;
+  }
+
+  bindEvents() {
+    PubSub.publish('InstrumentFamilies:data-ready', this.data);
+
+    PubSub.subscribe('SelectView:change', (evt) => {
+      const selectedIndex = evt.detail;
+      this.publishFamilyDetail(selectedIndex);
+    });
+  };
+
+  publishFamilyDetail(selectedIndex) {
+    const selectedFamily = this.data[selectedIndex];
+    PubSub.publish('InstrumentFamilies:selected-family-ready', selectedFamily)
+  };
+
+}
+
+// InstrumentFamilies.prototype.bindEvents = function () {
+//   PubSub.publish('InstrumentFamilies:data-ready', this.data);
+//
+//   PubSub.subscribe('SelectView:change', (evt) => {
+//     const selectedIndex = evt.detail;
+//     this.publishFamilyDetail(selectedIndex);
+//   });
+// };
+
+// InstrumentFamilies.prototype.publishFamilyDetail = function (selectedIndex) {
+//   const selectedFamily = this.data[selectedIndex];
+//   PubSub.publish('InstrumentFamilies:selected-family-ready', selectedFamily)
+// };
+
+export default InstrumentFamilies;
---
+++ hw_es6_instruments_start/client/src/views/select_view.js
@@ -0,0 +1,65 @@
+// const PubSub = require('../helpers/pub_sub.js');
+import PubSub from '../helpers/pub_sub.js';
+
+// const SelectView = function (element) {
+//   this.element = element;
+// };
+
+class SelectView {
+  constructor(element) {
+    this.element = element;
+  };
+
+  bindEvents () {
+    PubSub.subscribe('InstrumentFamilies:data-ready', (evt) => {
+      const allInstrumentFamilies = evt.detail;
+      this.populate(allInstrumentFamilies);
+    });
+
+    this.element.addEventListener('change', (evt) => {
+      const selectedIndex = evt.target.value;
+      PubSub.publish('SelectView:change', selectedIndex);
+    });
+  };
+
+  populate(instrumentFamilyData) {
+      instrumentFamilyData.forEach((familiy, index) => {
+        const option = document.createElement('option');
+        option.textContent = familiy.name;
+        option.value = index;
+        this.element.appendChild(option);
+      });
+    }
+};
+
+// SelectView.prototype.bindEvents = function () {
+//   PubSub.subscribe('InstrumentFamilies:data-ready', (evt) => {
+//     const allInstrumentFamilies = evt.detail;
+//     this.populate(allInstrumentFamilies);
+//   });
+//
+//   this.element.addEventListener('change', (evt) => {
+//     const selectedIndex = evt.target.value;
+//     PubSub.publish('SelectView:change', selectedIndex);
+//   });
+// };
+
+// SelectView.prototype.populate = function (instrumentFamilyData) {
+//   instrumentFamilyData.forEach((familiy, index) => {
+//     const option = document.createElement('option');
+//     option.textContent = familiy.name;
+//     option.value = index;
+//     this.element.appendChild(option);
+//   });
+// };
+
+// populate(instrumentFamilyData) {
+//     instrumentFamilyData.forEach((familiy, index) => {
+//       const option = document.createElement('option');
+//       option.textContent = familiy.name;
+//       option.value = index;
+//       this.element.appendChild(option);
+//     });
+// };
+
+export default SelectView;
